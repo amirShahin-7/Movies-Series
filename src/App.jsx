b1ae127be696895cc234a47ca56fefe1faa46a14
amirShahin-7 <amirshahin.1.512@gmail.com>
2025-04-25 07:23:00 +0300

    Update: Series + SeriesDetails + contactUs + Favicon in public

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Movies from "./pages/movies/Movies";
 import Series from "./pages/series/Series";
 import ContactUs from "./pages/contactUs/ContactUs";
+import SeriesDetails from "./pages/series/SeriesDetails";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/movies" element={<Movies />} />
         <Route path="/series" element={<Series />} />
         <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/series/:id" element={<SeriesDetails />} />
       </Routes>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import{ forToggle } from "./slices/searchToggleSlice";
-
+import { forToggle } from "./slices/searchToggleSlice";
+import { seriesReducer } from "./slices/series/seriesSlice";
+import { seriesDetailsReducer } from "./slices/series/seriesDetailsSlice";
 export const store = configureStore({
-  reducer: { forToggle },
+  reducer: { forToggle, seriesReducer, seriesDetailsReducer },
 });
